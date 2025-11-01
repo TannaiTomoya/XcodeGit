c77836b1d90ed713058b0aae57ee5d50233d6795
Tomoya <tomobou5912@gmail.com>
2025-11-02 08:34:28 +0900

    Initial Commit

---
+++ xcodeGit/xcodeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeGitApp.swift
+//  xcodeGit
+//
+//  Created by 丹内智弥 on 2025/11/02.
+//
+
+import SwiftUI
+
+@main
+struct xcodeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
